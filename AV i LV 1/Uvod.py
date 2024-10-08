6c420e1545ec30a616f55ade77f4ebe2b5a00a4b
tamiroladejo <koltam09@gmail.com>
2024-10-08 22:59:51 +0200

    Ispravljen Uvod.py

--- AV i LV 1/Uvod.py
+++ AV i LV 1/Uvod.py
@@ -1,9 +1,10 @@
-def faktorijel(n: int) -> int:
+def faktorijel(n: int) -> int: # Ovako profa deklarise funkcije, zanimljivo tbh. Nismo ovako radili kod Elvedina
     f = 1
     for i in range(1, n+1):
         f = f * i
     return f
 
+
 print(f"f(5) = {faktorijel(5)}")
-print(f"{1/3:.20f}") # format za ispis sa 20 decimala, vidimo da sve poslije 16. decimale nije tacno
-print(f"1.1 + 2.2:20f") # ovdje se isto desi, nakon 16. decimale nije tacan ispis
+print(f"{1/3:.20f}") # Format za ispis sa 20 decimala, Vidimo da sve poslije 16. decimale nije tacno
+print(f"1.1 + 2.2:20f") # Ovdje se isto desi, nakon 16. Decimale nije tacan ispis
