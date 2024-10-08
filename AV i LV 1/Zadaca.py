26b2c4cccf26f7e8fc5e2463de4ab7d854efd91b
tamiroladejo <koltam09@gmail.com>
2024-10-09 00:13:58 +0200

    Uradjeno pod a) i pod b)

--- AV i LV 1/Zadaca.py
+++ AV i LV 1/Zadaca.py
@@ -1,14 +1,36 @@
+# 1) Napraviti izracun e^x za x iz (-1,1) na 5 tacnih decimala
+# 2) Napraviti izracun za sinx, cosx, x iz (-1,1) na 5 tacnih decimala
+# 3) Racunanje korijena iz a (sqrt(a))
+# Pored toga izracunati ln(2) na 5 tacnih decimala
+
 def faktorijel(n: int) -> int:
     f = 1
     for i in range(1, n+1):
         f = f * i
     return f
 
-def eksponencijalna(x):
-    suma = 0.0
-    for i in range(0, 15):
-        suma = x**i/faktorijel(x) + suma
+
+def eksponencijalnaFunkcija(x: float) -> float: # Bit ce prvih 5 decimala tacno
+    suma = 0
+    for i in range(0, 9): # Ovdje se petlja mora ponoviti najmanje 9 puta da bi 5 decimala bilo tacno (Maklorenov red)
+        suma += x**i/faktorijel(i)
     return suma
 
 
-print(eksponencijalna(1.0))
+def sin(x: float) -> float: # Bit ce prvih 5 decimala tacno
+    suma = 0
+    for i in range(0, 9): # Ovdje se petlja mora ponoviti najmanje 9 puta da bi 5 decimala bilo tacno (Maklorenov red)
+        suma += (((-1)**i)*(x**((2*i)+1)))/faktorijel((2*i)+1)
+    return suma
+
+
+def cos(x: float) -> float: # Bit ce prvih 5 decimala tacno
+    suma = 0
+    for i in range(0, 9): # Ovdje se petlja mora ponoviti najmanje 9 puta da bi 5 decimala bilo tacno (Maklorenov red)
+        suma += (((-1)**i)*(x**(2*i)))/faktorijel(2*i)
+    return suma
+
+
+print(f"{eksponencijalnaFunkcija(-1/2):.5f}")
+print(f"{sin(-1/2):.5f}")
+print(f"{cos(-1/2):.5f}")
