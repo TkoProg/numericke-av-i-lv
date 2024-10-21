e44ea06b3660013873a7ca939e56d83d65db2961
tamiroladejo <koltam09@gmail.com>
2024-10-21 12:39:32 +0200

    Zadatak za samostalni rad AV i LV 3.

---
+++ AV i LV 3/Samostalni.py
@@ -0,0 +1,34 @@
+def ln(x: float) -> float:
+    suma = 0
+    temp = x
+    for i in range(0, 25):
+        suma += (((-1)**i)*(temp))/(i+1)
+        temp *= x
+    return suma
+
+def lnbolji(x: float) -> float:
+    suma = 0
+    temp = x
+    for i in range(1, 25):
+        suma += 2*(temp)/((2*i)-1)
+        temp *= x
+        temp *= x
+    return suma
+
+def lnlista(x: float) -> float:
+    suma = 0
+    temp = x
+    lista = []
+    for i in range(1, 25):
+        clan = (temp)/((2*i)-1)
+        temp *= x
+        temp *= x
+        lista.append(clan)
+    lista.reverse()
+    for i in range(1, 24):
+        suma += 2*lista[i]
+    return suma
+
+print(ln(1))
+print(lnbolji(1/3))
+print(lnlista(1/3))
